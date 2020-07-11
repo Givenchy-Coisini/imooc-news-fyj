d7fe13008c9515d2618f9da58526e9d829000df3
Fuyj <2312696405@qq.com>
2020-07-11 08:02:39 +0800

    搜索历史完成

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -4,14 +4,21 @@
 			<!-- 状态栏 -->
 			<view :style="{height: statusBarHeight+'px'}"> </view>
 			<!-- 导航栏 -->
-			<view class="navbar_context" :style="{height:navbarHeight+'px',width:windowWidth+'px'}">
-				<view class="navbar-search">
+			<view class="navbar_context" :style="{height:navbarHeight+'px',width:windowWidth+'px'}" @click.stop="open" :class="{search:isSearch}">
+				<view class="navbar_content_search_icons">
+					<uni-icons type="back" size="22" color="#fff" ></uni-icons>
+				</view>
+				<view v-if="!isSearch" class="navbar-search">
 					<view class="navbar-search_iocn">
 						<uni-icons type="search" size="18" color="#999"></uni-icons>
 					</view>
 					<view class="navbar-search_text">
 						uniapp、vue
 					</view>
+				</view>
+				<view  v-else class="navbar-search">
+					<!-- 搜索页面显示 -->
+					<input class="navbar-search_text" type="text" value="" placeholder="请输入您要搜索的内容"/>
 				</view>
 			</view>
 
@@ -24,10 +31,16 @@
 
 <script>
 	export default {
+		props: {
+			isSearch: { //判断这个组件是在搜索页还是在首页
+				type: Boolean,
+				default: false
+			}
+		},
 		data() {
 			return {
 				statusBarHeight: 20,
-				navbarHeight: 45,
+				navbarHeight: 45, 
 				windowWidth: 375
 			};
 		},
@@ -45,9 +58,17 @@
 			//(胶囊底部高度-状态栏高度)+(胶囊顶部高度-状态栏的高度)=导航栏的高度
 			this.navbarHeight = (menuButtonInfo.bottom - info.statusBarHeight) + (
 				menuButtonInfo.top - info.statusBarHeight)
-				console.log(this.navbarHeight)
+			console.log(this.navbarHeight)
 			this.windowWidth = menuButtonInfo.left
 			//#endif
+		},
+		methods: {
+			open() {
+				if(this.isSearch) return 
+				uni.navigateTo({
+					url: '/pages/home-search/home-search'
+				})
+			}
 		}
 	}
 </script>
@@ -88,8 +109,20 @@
 					}
 
 					.navbar-search_text {
-						font-size: 12px;
+						font-size: 14px;
 						color: #999;
+					}
+				}
+
+				&.search {
+					padding-left: 0;
+					.navbar_content_search_icons {
+						margin-left: 10px;
+						margin-right: 10px;
+					}
+
+					.navbar-search {
+						border-radius: 5px;
 					}
 				}
 			}
---
+++ pages/home-search/home-search.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="home">
+		<navbar :isSearch="true"></navbar>
+		<view class="home-list">
+			<view class="label_box">
+				<view class="label_header">
+					<text class="label-title">搜索历史</text>
+					<text class="label-clear">清空</text>
+				</view>
+				<view class="label-content">
+					<view v-for="item in 10" class="label-content-item">
+						{{item}}内容
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	page {
+		display: flex;
+		height: 100%;
+		background-color: #f5f5f5;
+	}
+
+	.home {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		border: 1px red solid;
+
+		.label_box {
+			background-color: #fff;
+
+			margin-bottom: 10px;
+
+			.label_header {
+				display: flex;
+				justify-content: space-between;
+				font-size: 16px;
+				color: #666;
+				padding: 10px 15px;
+				border-bottom: 1px #f5f5f5 solid;
+
+				.label-title {
+					color: $mk-base-color;
+				}
+
+				.label-clear {
+					color: #30b33a;
+					font-weight: bold;
+				}
+
+			}
+
+			.label-content {
+				display: flex;
+				flex-wrap: wrap;
+				padding: 15px;
+				padding-top: 0;
+				.label-content-item{
+				 padding: 2px 10px;
+				 margin-top: 12px;
+				 border: 1px #666 solid;
+				 font-size: 14px;
+				 color: #666;
+				 margin-right: 10px;
+				 border-radius: 5px;
+			}}
+		}
+	}
+</style>
